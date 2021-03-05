e8144ca4e90f098a9fdc43efa5872c2c22cb03d7
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-03-05 18:09:55 +0300

    first

---
+++ my_sin/tests/tests.cpp
@@ -0,0 +1,42 @@
+﻿// my_sin.cpp: определяет точку входа для приложения.
+//
+
+#include <gtest/gtest.h>
+#include "my_sin.h"
+#define PI 3.1416
+
+TEST(sinlib_test, for_zero)
+{
+    double from_lib = testspace::Sin(0);
+
+ 
+    ASSERT_NEAR(from_lib, 0, 0.0001);
+}
+
+TEST(sinlib_test, for_half_pi)
+{
+
+    double from_lib = testspace::Sin(PI/2);
+
+    ASSERT_NEAR(from_lib, 1, 0.0001);
+}
+
+TEST(sinlib_test, for_3div2_pi)
+{
+    double from_lib = testspace::Sin(PI*3/2);
+
+    ASSERT_NEAR(from_lib, -1, 0.0001);
+
+}
+TEST(sinlib_test, for_pi_div_4)
+{
+    double from_lib = testspace::Sin(PI/ 4);
+
+    ASSERT_NEAR(from_lib, 0.7071068, 0.0001);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
